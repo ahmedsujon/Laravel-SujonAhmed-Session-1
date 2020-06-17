85d1bc344bb6dbc37fb983600fb843c8bcb4b0db
ahmedsujon <ahmedsujon050@gmail.com>
2020-06-17 17:28:40 +0600

    Consultation MC

---
+++ app/Models/Consultation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Consultation extends Model
+{
+    protected $fillable = [
+        'name', 'phone', 'service', 'message',
+    ];
+}
+
--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/')->namespace('Web\App')->group(function () {
+
+    Route::resource('consultations', 'ConsultationController');
+
+});
+
+
+// Route::prefix('/')->namespace('Web\Admin')->group(function () {
+
+//     Route::resource('schools', 'SchoolController');
+
+// });
+
+
 
 Auth::routes();
 
